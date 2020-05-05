ffb38b12265d8fe87af8715575a0caa0d5d14618
uilnauyis <uilnauyis@gmail.com>
2020-05-05 13:00:28 +1200

    Improve Downloading data via DEE2 R interface

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # homosapien-DEE2-data
 
-
 TODO:
 
-- [ ] Update the code that downloads DEE2 data to get all the hsapien data and format it.
+- [x] Update the code that downloads DEE2 data to get all the hsapien data and format it.
 - [ ] Check if any extra normalization methods should be applied to the collected data.
 - [ ] Check if it is necessary to remove batch effect
 - [ ] t-SNE analysis of the data
-- [ ] table summary of the pheno data 
+- [ ] table summary of the pheno data
 - [ ] try different iqr threshold and different categories in PCA analysis.
--- constructSExprFromDEE2Data.R
+++ constructSExprFromDEE2Data.R
@@ -1,20 +1,28 @@
 library("SummarizedExperiment")
-library("readODS")
+library("rio")
+library("getDEE2")
 
 constructSExprFromDEE2Data <- function() {
+    unzip('./SelectedDEE2_v2.zip', exdir = 'SelectedDEE2_v2_csv')
+
     ## get counts
-    geneCountsDFrame <- read.table(file = "./DEE2Data/GeneCountMatrix.tsv", sep = '\t', 
-        row.names=1, header=TRUE)
+    geneCountsDFrame <- read.table(
+        file = "./SelectedDEE2_v2_csv/GeneCountMatrix.tsv", 
+        sep = '\t', 
+        row.names=1, 
+        header=TRUE)
 
     counts <- data.matrix(geneCountsDFrame)
 
     ## get rowData
-    geneInfoDFrame <- read.table(file = "./DEE2Data/GeneInfo.tsv", sep = '\t', 
-        row.names=1, header=TRUE)[, "GeneSymbol", drop=FALSE]
+    geneInfoDFrame <- read.table(
+        file = "./SelectedDEE2_v2_csv/GeneInfo.tsv", 
+        sep = '\t', 
+        row.names=1, 
+        header=TRUE)[, "GeneSymbol", drop=FALSE]
 
     ## get colData
-    samplesDFrame <- read_ods(path = "./DEE2Data/SelectedDEE2_v2.ods", 
-        sheet = 1, col_names = TRUE)
+    samplesDFrame <- import("./SelectedDEE2_v2.ods")
 
     samplesDFrame <- samplesDFrame[!duplicated(samplesDFrame[, "SRR_accession"]), ]
 
@@ -28,9 +36,21 @@ constructSExprFromDEE2Data <- function() {
     validSamplesDFrame <- validSamplesDFrame[validSamplesSRRAccessions, ]
 
     ## construct sExpt object for the selected DEE2 data
-    SummarizedExperiment(assays=list(counts=counts),
+    sExpr <- SummarizedExperiment(assays=list(counts=counts),
                          rowData=geneInfoDFrame, colData=validSamplesDFrame)
+
+    saveRDS(sExpr, "./selectedDEE2SExpr.rds")
+
+    sExpr
 }
 
+downloadDEE2Data <- function() {
+    dFrame <- import('./SelectedDEE2_v2.ods')
+    SRR_accessions <- unique((dFrame[['SRR_accession']]))
+
+    dee2Data <- getDEE2('hsapiens', SRR_accessions, 
+        outfile='./SelectedDEE2_v2')
+}
+
+downloadDEE2Data()
 sExpr <- constructSExprFromDEE2Data()
-saveRDS(sExpr, "./rdsFiles/selectedDEE2SExpr.rds")
